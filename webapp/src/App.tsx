488737bf1d2aafc35ee4e991eabba4508b830ef2
uo282492 <uo282492@uniovi.es>
2023-03-25 22:18:16 +0100

    Añadido Router a App

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,26 +4,31 @@ import {QueryClient, QueryClientProvider} from "react-query";
 import LoginPage from './components/login/LoginPage';
 import {SessionProvider, useSession} from "@inrupt/solid-ui-react";
 import MainPage from "./components/MainPage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App(): JSX.Element {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
-  //With this we can control the login status for solid
-  const { session } = useSession();
+	//With this we can control the login status for solid
+ 	const { session } = useSession();
 
-  const queryClient = new QueryClient();
+	const queryClient = new QueryClient();
 
-  session.onLogin(() => setIsLoggedIn(true));
-  session.onLogout(() => setIsLoggedIn(false));
+	session.onLogin(() => setIsLoggedIn(true));
+	session.onLogout(() => setIsLoggedIn(false));
 
-  return(
-      <SessionProvider>
-        {(!isLoggedIn) ? <LoginPage /> : <QueryClientProvider client={queryClient}> <MainPage session={session}/> </QueryClientProvider>}
-      </SessionProvider>
-  )
-
+	return(
+		<SessionProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<LoginPage/>} />
+					<Route path="/map" element={<QueryClientProvider client={queryClient}><MainPage session={session}/></QueryClientProvider>} />
+				</Routes>
+			</Router>
+		</SessionProvider>
+	);	
 }
 
 export default App;
