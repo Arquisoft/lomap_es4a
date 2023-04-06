13a83dc9563e9ce7acb830f418c9a7eddeb3b735
uo282492 <uo282492@uniovi.es>
2023-04-06 13:13:03 +0200

    Añadida vista de eliminar mapa

--- webapp/src/components/Navbar/MapListView.tsx
+++ webapp/src/components/Navbar/MapListView.tsx
@@ -7,6 +7,7 @@ import ListItemText from '@mui/material/ListItemText';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import FindReplaceIcon from '@mui/icons-material/FindReplace';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { SelectChangeEvent, InputLabel, MenuItem, Select, FormControl, createTheme, ThemeProvider, IconButton, Divider, TextField } from "@mui/material";
 
 
@@ -17,23 +18,35 @@ interface MapListViewProps {
 
 function MapListView(props: MapListViewProps): JSX.Element {
 
-    const [currentMap, setCurrentMap] = useState("");
-    const [currentNewMap, setCurrentNewMap] = useState("");
+    const [currentLoadMap, setCurrentLoadMap] = useState(""); // info del mapa a cargar
+    const [currentNewMap, setCurrentNewMap] = useState(""); // info del mapa a crear
+    const [currentDeleteMap, setCurrentDeleteMap] = useState(""); // info del mapa a borrar
 
-    const handleMapChange = (event: SelectChangeEvent) => {
-        setCurrentMap(event.target.value);
+    const handleLoadMapChange = (event: SelectChangeEvent) => {
+        setCurrentLoadMap(event.target.value);
     };
     
     const handleNewMapChange = (event: any) => {
         setCurrentNewMap(event.target.value);
     };
 
+    const handleDeleteMapChange = (event: SelectChangeEvent) => {
+        setCurrentDeleteMap(event.target.value);
+    };
+
     const handleLoadMapClick = () => {
         // TODO: Se debe de cargar la lista de puntos correspondiente al mapa seleccionado
+        setCurrentLoadMap("");
         props.onClose();
     };
 
-    const handleCreateMapClick = () => {
+    const handleDeleteMapClick = () => {
+        // TODO: Se debe de eliminar el mapa seleccionado
+        setCurrentDeleteMap("");
+        props.onClose();
+    };
+
+    const handleNewMapClick = () => {
         // TODO: Se debe de crear el nuevo mapa con el nombre escogido (y validar el nuevo nombre)
         if (currentNewMap != null && currentNewMap.trim() !== "") {
             setCurrentNewMap("");
@@ -86,7 +99,7 @@ function MapListView(props: MapListViewProps): JSX.Element {
                         fullWidth
                         variant="contained"
                         sx={{ mb: 4 }}
-                        onClick={handleCreateMapClick}
+                        onClick={handleNewMapClick}
                     >
                         Create new Map
                     </Button>
@@ -104,8 +117,8 @@ function MapListView(props: MapListViewProps): JSX.Element {
                                 labelId="selectMapLabel"
                                 label="Choose a map to load"
                                 id="selectMap"
-                                value={currentMap}
-                                onChange={handleMapChange}
+                                value={currentLoadMap}
+                                onChange={handleLoadMapChange}
                             >
                                 <MenuItem value={"Mapa1"}>Mapa1</MenuItem>
                                 <MenuItem value={"Mapa2"}>Mapa2</MenuItem>
@@ -120,9 +133,44 @@ function MapListView(props: MapListViewProps): JSX.Element {
                         type="submit"
                         fullWidth
                         variant="contained"
+                        sx={{ mb: 4 }}
                         onClick={handleLoadMapClick}
                     >
                         Load Map
+                    </Button>
+                </ListItem>
+                <Divider sx={{backgroundColor: "#808b96", height: "0.1em"}} />
+                <ListItem>
+                    <DeleteIcon />
+                    <ListItemText primary="Delete an existent map" />
+                </ListItem>
+                <ListItem>
+                    <ThemeProvider theme={darkTheme}>
+                        <FormControl fullWidth >
+                            <InputLabel id="selectDeleteMapLabel">Choose a map to delete</InputLabel>                        
+                            <Select 
+                                labelId="selectDeleteMapLabel"
+                                label="Choose a map to delete"
+                                id="selectDeleteMap"
+                                value={currentDeleteMap}
+                                onChange={handleDeleteMapChange}
+                            >
+                                <MenuItem value={"Mapa1"}>Mapa1</MenuItem>
+                                <MenuItem value={"Mapa2"}>Mapa2</MenuItem>
+                                <MenuItem value={"Mapa3"}>Mapa3</MenuItem>
+                                <MenuItem value={"Mapa4"}>Mapa4</MenuItem>
+                            </Select>                        
+                        </FormControl>
+                    </ThemeProvider>
+                </ListItem>
+                <ListItem>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={handleDeleteMapClick}
+                    >
+                        Delete Map
                     </Button>
                 </ListItem>
             </List>
