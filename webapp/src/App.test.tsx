7c915d2fe33827a4d42cd51596cc8e51a93b545d
uo282492 <uo282492@uniovi.es>
2023-04-09 15:37:28 +0200

    Arreglado bug en los tests de LoginPage y App

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {act} from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
+import "@inrupt/jest-jsdom-polyfills";
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => { render(<App />); });
   const linkElement = screen.getByText("Log In");
   expect(linkElement).toBeInTheDocument();
 });
