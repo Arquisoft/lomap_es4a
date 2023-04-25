ff58ff4c99343dce46c63176156f83c8be71ac4f
uo282492 <uo282492@uniovi.es>
2023-04-25 14:42:40 +0200

    Fichero para conceder permisos

---
+++ webapp/src/solidapi/permissions.ts
@@ -0,0 +1,65 @@
+import {
+    setAgentDefaultAccess,getSolidDatasetWithAcl,
+    hasResourceAcl,
+    hasFallbackAcl,
+    hasAccessibleAcl,
+    createAcl,
+    createAclFromFallbackAcl,
+    getResourceAcl,
+    setAgentResourceAccess,
+    saveAclFor
+} from "@inrupt/solid-client";
+import { Session } from "@inrupt/solid-client-authn-browser";
+
+async function permissions(session: Session, friendsWebIds:string[]) {
+
+    if (session.info.webId === null || typeof session.info.webId === "undefined")
+        throw new Error("Invalid session");
+
+    const lomapUrl = session.info.webId.split("/").slice(0, 3).join("/").concat("/public", "/lomap");
+
+    // Fetch the SolidDataset and its associated ACLs, if available:
+    const myDatasetWithAcl = await getSolidDatasetWithAcl(lomapUrl);
+
+    // Obtain the SolidDataset's own ACL, if available,
+    // or initialise a new one, if possible:
+    let resourceAcl;
+    if (!hasResourceAcl(myDatasetWithAcl)) {
+        if (!hasAccessibleAcl(myDatasetWithAcl)) {
+            throw new Error(
+            "The current user does not have permission to change access rights to this Resource."
+            );
+        }
+        if (!hasFallbackAcl(myDatasetWithAcl)) {
+            // Initialise a new empty ACL
+            resourceAcl = createAcl(myDatasetWithAcl);
+        } else {
+            resourceAcl = createAclFromFallbackAcl(myDatasetWithAcl);
+        }
+    } else {
+        resourceAcl = getResourceAcl(myDatasetWithAcl);
+    }
+    
+
+    // Give yourself Control access to the given Resource:
+    let updatedAcl = setAgentResourceAccess(
+        resourceAcl,
+        session.info.webId,
+        { read: true, append: true, write: true, control: true }
+    );
+    // Now save the ACL:
+    await saveAclFor(myDatasetWithAcl, updatedAcl);
+
+
+    // Give your friends Control access to the given Resource:
+    for (let friendWebId of friendsWebIds) {
+        updatedAcl = setAgentResourceAccess(
+            updatedAcl,
+            friendWebId,
+            { read: true, append: true, write: true, control: true }
+        );
+        // Now save the ACL:
+        await saveAclFor(myDatasetWithAcl, updatedAcl);
+    }      
+}
+  
