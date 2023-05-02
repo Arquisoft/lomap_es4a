9fb380421c93474bf028f2ce0fcdfc87bb77447a
uo282492 <uo282492@uniovi.es>
2023-05-03 00:57:03 +0200

    Refactorizados PointsViewTest

--- webapp/src/components/Navbar/PointsView.test.tsx
+++ webapp/src/components/Navbar/PointsView.test.tsx
@@ -4,191 +4,118 @@ import "@inrupt/jest-jsdom-polyfills";
 import PointsView from './PointsView';
 import { Marker } from "@googlemaps/jest-mocks";
 
+let open:boolean;
+
+beforeEach( () => { 
+    const functionMock = jest.fn();
+    open = true;
+    const close = () => open = false;
+    
+    let markerList = {
+        'marker1': new Marker({ position: { lat: 40.7128, lng: -74.0060 } }),
+        'marker2': new Marker({ position: { lat: 37.7749, lng: -122.4194 } })
+    };
+    
+    render(<PointsView open={open} onClose={close} markerList={markerList} 
+        openEditPoint={functionMock} deletePoint={functionMock}  
+        getPointsCategory={functionMock}></PointsView>);
+});
+
+
+// Clicka el elemento con id pasado
+function clickButton(id: string): void {
+    const button = screen.getByTestId(id);
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+}
+
 
 test('check pointsView renders correctly', async () => {
-    const functionMock = jest.fn();
-    
-    const { getByLabelText } = await render(<PointsView open={true} onClose={functionMock} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    
     expect(await screen.findByText("Points List")).toBeInTheDocument();
 });
 
 test('check pointsView closes correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
-    
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("closePointsView");
-    fireEvent.click(button);
+    clickButton("closePointsView");
     expect(open).toBeFalsy();
     
 });
 
 test('check pointsView filters subbmenu opens correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
-    
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("filters");
-    fireEvent.click(button);
-    const filtersCategories = await getByTestId("filtersCategories");
+    clickButton("filters");
+    const filtersCategories = screen.getByTestId("filtersCategories");
     expect(filtersCategories).toBeInTheDocument();
 
 });
 
 test('check pointsView category filter subbmenu opens correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
-    
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("filters");
-    fireEvent.click(button);
-    
-    const filtersCategories = await getByTestId("filtersCategories");
-    expect(filtersCategories).toBeInTheDocument();
-    fireEvent.click(filtersCategories);
-    
-    const filterButton = await getByTestId("filterButton");
+    clickButton("filters");    
+    clickButton("filtersCategories");
+
+    const filterButton = screen.getByTestId("filterButton");
     expect(filterButton).toBeInTheDocument();
 
 });
 
 test('check pointsView category markall and filter subbmenu opens correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
+    clickButton("filters");    
+    clickButton("filtersCategories");
+    clickButton("markall");
     
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("filters");
-    fireEvent.click(button);
-    
-    const filtersCategories = await getByTestId("filtersCategories");
-    expect(filtersCategories).toBeInTheDocument();
-    fireEvent.click(filtersCategories);
-
-    const markall = await getByTestId("markall");
-    expect(markall).toBeInTheDocument();
-    fireEvent.click(markall);
-    
-    const filterButton = await getByTestId("filterButton");
+    const filterButton = screen.getByTestId("filterButton");
     expect(filterButton).toBeInTheDocument();
 
 });
 
 test('check pointsView category unmarkall and filter subbmenu opens correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
+    clickButton("filters");    
+    clickButton("filtersCategories");
+    clickButton("unmarkall");
     
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("filters");
-    fireEvent.click(button);
-    
-    const filtersCategories = await getByTestId("filtersCategories");
-    expect(filtersCategories).toBeInTheDocument();
-    fireEvent.click(filtersCategories);
-
-    const unmarkall = await getByTestId("unmarkall");
-    expect(unmarkall).toBeInTheDocument();
-    fireEvent.click(unmarkall);
-    
-    const filterButton = await getByTestId("filterButton");
+    const filterButton = screen.getByTestId("filterButton");
     expect(filterButton).toBeInTheDocument();
 
 });
 
 test('check pointsView category marc academic and filter subbmenu opens correctly', async () => {
-    const handleFilterChange = jest.fn();
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
     
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("filters");
-    fireEvent.click(button);
-    
-    const filtersCategories = await getByTestId("filtersCategories");
-    expect(filtersCategories).toBeInTheDocument();
-    fireEvent.click(filtersCategories);
+    clickButton("filters");    
+    clickButton("filtersCategories");
 
-    const academiccheckbox = await getByTestId("academiccheckbox");
+    const academiccheckbox = screen.getByTestId("academiccheckbox");
     expect(academiccheckbox).toBeInTheDocument();
     const checkBox = within(academiccheckbox).getByRole("checkbox");
     fireEvent.click(checkBox);
 
-    const filterButton = await getByTestId("filterButton");
-    expect(filterButton).toBeInTheDocument();
-    fireEvent.click(filterButton);
+    clickButton("filterButton");
 });
 
 
 test('check pointsView visibility subbmenu opens correctly', async () => {
 
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
-    
-    const { getByTestId } = await render(<PointsView open={open} onClose={close} markerList={{}} openEditPoint={functionMock} deletePoint={functionMock}  getPointsCategory={functionMock}></PointsView>);
-    const button = await getByTestId("visibility");
-    fireEvent.click(button);
-
-     expect(await screen.findByText("Show / Hide all")).toBeInTheDocument();
+    clickButton("visibility");
+    expect(await screen.findByText("Show / Hide all")).toBeInTheDocument();
 
 });
 
 
 test('check pointsView visibility all button switches correctly', async () => {
    
-    let open = true;
-    const close = () => {
-        open = false;
-    };
-    const functionMock = jest.fn();
-  const { getByTestId } = render(<PointsView
-      open={open}
-      onClose={close}
-      markerList={{
-          'marker1': new Marker({ position: { lat: 40.7128, lng: -74.0060 } }),
-          'marker2': new Marker({ position: { lat: 37.7749, lng: -122.4194 } })
-      }}
-      openEditPoint={functionMock}
-      deletePoint={functionMock}
-      getPointsCategory={functionMock} />);
-    const button = getByTestId("visibility");
-    fireEvent.click(button);
-    const visibilityall = getByTestId("visibilityall");
+    clickButton("visibility");
+
+    const visibilityall = screen.getByTestId("visibilityall");
     const suich = within(visibilityall).getByRole("checkbox");
     fireEvent.click(suich);
 
-    const suich2 = getByTestId("marker1");
+    const suich2 = screen.getByTestId("marker1");
     const suich22 = within(suich2).getByRole("checkbox");
     fireEvent.click(suich22);
 
-    const buttonEditar = getByTestId("editarmarker1");
-    fireEvent.click(buttonEditar);
+    clickButton("editarmarker1");
 
-    const buttonBorrar = getByTestId("borrarmarker1");
-    fireEvent.click(buttonBorrar);
+    clickButton("borrarmarker1");
 });
