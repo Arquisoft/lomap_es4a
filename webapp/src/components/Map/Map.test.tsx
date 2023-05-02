ac6c59e63eb943badbdbbec6f45ea897c4dd57ed
UO271477 <uo271477@uniovi.es>
2023-05-02 16:22:54 +0200

    Arreglo Smells sonar

--- webapp/src/components/Map/Map.test.tsx
+++ webapp/src/components/Map/Map.test.tsx
@@ -1,9 +1,7 @@
 import "@inrupt/jest-jsdom-polyfills";
-import {act, fireEvent, render, screen} from '@testing-library/react';
-import {Session} from "@inrupt/solid-client-authn-browser";
+import {act, render, screen} from '@testing-library/react';
 import Mapa from "./Map";
-import * as solidApi from "../../solidapi/solidapi";
-import Point from "../../solidapi/Point";
+
 
 test('check initial map fails to render', async() => {
     await act(async () => {
