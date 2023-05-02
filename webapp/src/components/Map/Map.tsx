fb5616c5a0e483060c73f38c9cb0ae83643f44c5
UO271477 <uo271477@uniovi.es>
2023-05-02 16:05:18 +0200

    Map Coverage 100%

--- webapp/src/components/Map/Map.tsx
+++ webapp/src/components/Map/Map.tsx
@@ -27,25 +27,13 @@ function Mapa({session, markers, markerList, clickMap, clickMarker, setMarkerToA
         // eslint-disable-next-line
         }, [currentMapName]);
    
-    const addMarker=(pointId: string, m:google.maps.Marker)=>{
-        mList[pointId] = m;
-    }
+    const addMarker=(pointId: string, m:google.maps.Marker)=>{ mList[pointId] = m; }
 
-    const onLoad = (googleMap: google.maps.Map): void => {
-        createMap(session, currentMapName).then(() => {retrievePoints(session, currentMapName).then(points => {if (points != null) {points.forEach(point => {let marker = new google.maps.Marker({position: {lat: point.latitude, lng: point.longitude}, map: googleMap, title: point.name, icon: {url: savedMarker2}});marker.setMap(googleMap);marker.setMap(googleMap);marker.addListener('click', (marker: any) =>{clickMarker(marker.latLng.lat(), marker.latLng.lng());});addMarker(point.id, marker);});setMap(googleMap);markerList(mList);}});
-        });
-    }
+    const onLoad = (googleMap: google.maps.Map): void => {createMap(session, currentMapName).then(() => {retrievePoints(session, currentMapName).then(points => {if (points != null) {points.forEach(point => {let marker = new google.maps.Marker({position: {lat: point.latitude, lng: point.longitude}, map: googleMap, title: point.name, icon: {url: savedMarker2}});marker.setMap(googleMap);marker.setMap(googleMap);marker.addListener('click', (marker: any) =>{clickMarker(marker.latLng.lat(), marker.latLng.lng());});addMarker(point.id, marker);});setMap(googleMap);markerList(mList);}});});}
 
-    const onUnMount = (): void => {
-        setMap(null);
-    };
-
-    const onMapClick = (e: google.maps.MapMouseEvent) => {
-        // @ts-ignore
-        if (e.latLng != null) {let marker = new google.maps.Marker({position: {lat: e.latLng.lat(), lng: e.latLng.lng()}, map: map, title: 'Prueba save', icon: {url: savedMarker,}, visible:true,});marker.addListener('click', (marker: any) =>{clickMarker(marker.latLng.lat(), marker.latLng.lng()); }); setMarkerToAdd(marker); clickMap(e.latLng.lat(), e.latLng.lng());
-        }
-
-    };
+    const onUnMount = (): void => {setMap(null);};
+    // @ts-ignore
+    const onMapClick = (e: google.maps.MapMouseEvent) => {if (e.latLng != null) {let marker = new google.maps.Marker({position: {lat: e.latLng.lat(), lng: e.latLng.lng()}, map: map, title: 'Prueba save', icon: {url: savedMarker,}, visible:true,});marker.addListener('click', (marker: any) =>{clickMarker(marker.latLng.lat(), marker.latLng.lng()); }); setMarkerToAdd(marker); clickMap(e.latLng.lat(), e.latLng.lng());}};
 
     if(!isLoaded) return <div>Map loading...</div>;return(<div><GoogleMap data-testid={"mapToTest"} id="map" mapContainerStyle={containerStyle} options={options as google.maps.MapOptions} center={center} zoom={10} onLoad={onLoad} onUnmount={onUnMount} onClick={onMapClick}></GoogleMap></div>);
 }
