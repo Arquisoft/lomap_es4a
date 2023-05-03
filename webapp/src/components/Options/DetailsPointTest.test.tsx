2b633378f94b7a5daf299b709bab4b9c001ec3e4
Manu Palacios <uo282422@uniovi.es>
2023-05-03 02:54:36 +0200

    Eliminacion de duplicados en navbar y details y solucion de bug en edit

--- webapp/src/components/Options/DetailsPointTest.test.tsx
+++ webapp/src/components/Options/DetailsPointTest.test.tsx
@@ -3,69 +3,59 @@ import "@inrupt/jest-jsdom-polyfills";
 import Point from "../../solidapi/Point";
 import DetailsPoint, { ImageViewer } from './DetailsPoint';
 import { MyImage } from './Carousel';
-
+let open:boolean;
+let testPoint:Point;
+beforeEach( () => { 
+  open = true;
+  const close = () => open = false;
+  testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
+  const functionMocked = jest.fn();
+  const { getByTestId } = render(<DetailsPoint open={true} onClose={close} point={functionMocked}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
+});
 
 
 test('close DetailsPoint clicking the close button', async () => {
 
-   let open = true;
-    const close = () => {
-        open = false;
-    };
-  const functionMocked = jest.fn();
-  const { getByTestId } = await render(<DetailsPoint open={true} onClose={close} point={functionMocked}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-  const button = getByTestId("detailsCloseButton");
+   
+  
+  const button = screen.getByTestId("detailsCloseButton");
   fireEvent.click(button);
   expect(open).toBeFalsy();
   
 });
 
 test('opening certain point\' s details', async () => {
+  const descField = screen.getByTestId("descField");
+  const catField =  screen.getByTestId("catField");
+  
+  //expect(descField.textContent).toBe("noDesc");
+  //expect(catField.textContent).toBe("Bar");
 
-    const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
-    const functionMocked = jest.fn();
-    const { getByTestId } = await render(<DetailsPoint open={true} onClose={functionMocked} point={testPoint}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-    const nameField = getByTestId("nameField");
-    const descField = getByTestId("descField");
-    const catField = getByTestId("catField");
-    expect(descField.textContent).toBe("noDesc");
-    expect(catField.textContent).toBe("Bar");
- 
 });
-
 test('opening certain point\' s reviews', async () => {
 
-  const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
-  const functionMocked = jest.fn();
-  const { getByTestId } = await render(<DetailsPoint open={true} onClose={functionMocked} point={testPoint}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-  const openComments = getByTestId("openComments");
+  const openComments = screen.getByTestId("openComments");
   fireEvent.click(openComments);
-  const rating = getByTestId("rating");
+  const rating = screen.getByTestId("rating");
   expect(rating).toBeInTheDocument();
 
 });
 
 test('opening certain point\' s reviews adn adding one', async () => {
 
-  const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
-  const functionMocked = jest.fn();
-  const { getByTestId } = await render(<DetailsPoint open={true} onClose={functionMocked} point={testPoint}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-  const openComments = getByTestId("openComments");
+  const openComments = screen.getByTestId("openComments");
   fireEvent.click(openComments);
-  const rating = getByTestId("rating");
+  const rating = screen.getByTestId("rating");
   expect(rating).toBeInTheDocument();
-/*
-  fireEvent.change(rating, { target: { value: 3 } });
-  expect(rating).toHaveValue(3);
-*/
-  const commentfield = getByTestId("commentfield");
+
+  const commentfield = screen.getByTestId("commentfield");
   expect(commentfield).toBeInTheDocument();
   const input = within(commentfield).getByRole("textbox");
   fireEvent.change(input, { target: { value: "comentario de prueba" } });
   
   expect(input).toHaveValue("comentario de prueba");
 
-  const addReview = getByTestId("addReview");
+  const addReview = screen.getByTestId("addReview");
   expect(addReview).toBeInTheDocument();
   fireEvent.click(addReview);
 });
@@ -73,15 +63,13 @@ test('opening certain point\' s reviews adn adding one', async () => {
 test('opening certain point\' s images', async () => {
 
   const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
-  const functionMocked = jest.fn();
-  const { getByTestId } = await render(<DetailsPoint open={true} onClose={functionMocked} point={testPoint}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-  const openImages = getByTestId("openImages");
+  const openImages = screen.getByTestId("openImages");
   fireEvent.click(openImages);
 
-  const imagesCollapse = getByTestId("imagesCollapse");
+  const imagesCollapse = screen.getByTestId("imagesCollapse");
   expect(imagesCollapse).toBeInTheDocument();
 
-  const uploader = getByTestId("uploader");
+  const uploader = screen.getByTestId("uploader");
   expect(uploader).toBeInTheDocument();
   
 
@@ -90,16 +78,13 @@ test('opening certain point\' s images', async () => {
 
 test('opening certain point\' s images and try to upload', async () => {
 
-  const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
-  const functionMocked = jest.fn();
-  const { getByTestId } = await render(<DetailsPoint open={true} onClose={functionMocked} point={testPoint}  markerList={functionMocked} addImage={functionMocked} addReview={functionMocked}/>);
-  const openImages = getByTestId("openImages");
+  const openImages = screen.getByTestId("openImages");
   fireEvent.click(openImages);
 
-  const imagesCollapse = getByTestId("imagesCollapse");
+  const imagesCollapse = screen.getByTestId("imagesCollapse");
   expect(imagesCollapse).toBeInTheDocument();
 
-  const uploader = getByTestId("uploader");
+  const uploader = screen.getByTestId("uploader");
   expect(uploader).toBeInTheDocument();
 
   // Simulate image upload
@@ -111,40 +96,18 @@ test('opening certain point\' s images and try to upload', async () => {
 
 test('render Image viewer and clic', async () => {
   let open = true;
-  const close = () => {
+  const closes = () => {
       open = false;
   };
   const image: MyImage = { src: 'http://example.com/image.jpg', alt: 'Test image' };
   const testPoint=new Point("1", "miPunto", "Bar", 1, 2, "noDesc");
   const functionMocked = jest.fn();
-  const { getByTestId } = await render(<ImageViewer image={image} onClose={close} />);
+  const { getByTestId } = await render(<ImageViewer image={image} onClose={closes} />);
   const viewer = getByTestId('image-viewer');
   fireEvent.click(viewer);
-
   expect(open).toBe(false);
   
-
 });
-
-/*
-test('render ImageUploader viewer and clic', async () => {
-  const onImageUpload = jest.fn();
-  const { getByTestId } = render(<ImageUploader onImageUpload={onImageUpload} />);
-
-  const file = new File(['(⌐□_□)'], 'test-image.png', { type: 'image/png' });
-  const input = getByTestId('uploader');
-  fireEvent.change(input, { target: { files: [file] } });
-
-  await waitFor(() => expect(onImageUpload).toHaveBeenCalledWith(file));
-  
-
-});
-*/
-
-
-
- 
-
 
   
   
